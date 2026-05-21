7de2060cf76158a56f78f0e0d008419fca131221
marcosanz <marcosanz@grupotecopy.es>
2026-05-21 09:27:24 +0200

    Added Jitpack configuration

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,36 @@
+import com.android.build.api.dsl.LibraryExtension
+
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.android.library) apply false
     alias(libs.plugins.ksp) apply false
+}
+
+subprojects {
+    val subproject = this
+    plugins.withId("com.android.library") {
+        apply(plugin = "maven-publish")
+
+        extensions.configure<LibraryExtension> {
+            publishing {
+                singleVariant("release")
+            }
+        }
+
+        afterEvaluate {
+            extensions.configure<PublishingExtension> {
+                publications {
+                    register<MavenPublication>("release") {
+                        groupId = project.property("GROUP_ID").toString()
+                        artifactId = subproject.name
+                        version = project.property("LIBRARY_VERSION").toString()
+
+                        afterEvaluate {
+                            from(components["release"])
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
---
+++ gallery-core-ui/src/main/java/com/marcosanz/gallery_core_ui/util/GalleryFileProvider.kt
@@ -0,0 +1,5 @@
+package com.marcosanz.gallery_core_ui.util
+
+import androidx.core.content.FileProvider
+
+class GalleryFileProvider : FileProvider()
